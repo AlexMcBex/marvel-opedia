4cd9d336a11458398afa9ad1e3a25e2ca1791b1f
alexmcbex <alessiobelli94@gmail.com>
2024-09-16 15:24:32 -0400

    added BrowserRouter component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import React from "react";
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 
 function App() {
+  
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
-        <Route path="/characters" element={<div>Characters</div>} />
-        <Route path="/comics" element={<div>Comics</div>} />
-        <Route path="/creators" element={<div>Creators</div>} />
-        <Route path="/series" element={<div>Series</div>} />
-        <Route path="/stories" element={<div>Stories</div>} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes> 
-    </div>
-  );
+  <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<h1 className="">Home</h1>} />
+    <Route path="/about" element={<h1>About</h1>} />
+    <Route path="/characters" element={<h1>Characters</h1>} />
+    <Route path="/comics" element={<h1>Comics</h1>} />
+    <Route path="/creators" element={<h1>Creators</h1>} />
+    <Route path="/series" element={<h1>Series</h1>} />
+    <Route path="/stories" element={<h1>Stories</h1>} />
+    <Route path="*" element={<h1>404</h1>} />
+  </Routes>
+  </BrowserRouter>
+  )
 }
 
 export default App;
